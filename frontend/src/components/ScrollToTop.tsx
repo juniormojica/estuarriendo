9044e785c2deade7ad979d0c7d8caf35a7299fad
jr mojica <juniormojica26@gmail.com>
2026-03-28 10:35:15 -0500

    fix(ui): scroll to top of page on route and query param changes
    - Update ScrollToTop component to listen to search params as well as pathname
    
    - Add slight timeout to allow React DOM to render before scrolling

--- frontend/src/components/ScrollToTop.tsx
+++ frontend/src/components/ScrollToTop.tsx
@@ -2,11 +2,18 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
-    const { pathname } = useLocation();
+    const { pathname, search } = useLocation();
 
     useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [pathname]);
+        // Use timeout to ensure it runs after DOM elements have painted
+        setTimeout(() => {
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'instant'
+            });
+        }, 0);
+    }, [pathname, search]);
 
     return null;
 };
