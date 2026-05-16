8798aa9bc6d2d1ba99fcc244affc860eec5432ce
jr mojica <juniormojica26@gmail.com>
2026-05-15 20:42:16 -0500

    refactor(auth): extract property ownership service

--- backend/src/controllers/containerController.js
+++ backend/src/controllers/containerController.js
@@ -4,66 +4,7 @@ import { sequelize } from '../models/index.js';
 import { AppError, badRequest, forbidden, notFound } from '../errors/AppError.js';
 import { UserType } from '../utils/enums.js';
 import { ensureOwnUserOrAdmin } from '../utils/authorization.js';
-
-const assertContainerOwnership = async (req, containerId, { transaction, forbiddenMessage }) => {
-    const { Property } = await import('../models/index.js');
-    const container = await Property.findByPk(containerId, {
-        attributes: ['id', 'ownerId', 'isContainer'],
-        transaction
-    });
-
-    if (!container) {
-        throw notFound('Pensión/apartamento no encontrado', { code: 'CONTAINER_NOT_FOUND' });
-    }
-
-    if (!container.isContainer) {
-        throw badRequest('La propiedad no es una pensión/apartamento', { code: 'PROPERTY_NOT_CONTAINER' });
-    }
-
-    await ensureOwnUserOrAdmin(req, container.ownerId, {
-        forbiddenCode: 'CONTAINER_ACCESS_FORBIDDEN',
-        forbiddenMessage
-    });
-
-    return container;
-};
-
-const assertUnitOwnership = async (req, unitId, { transaction, forbiddenMessage }) => {
-    const { Property } = await import('../models/index.js');
-
-    const unit = await Property.findByPk(unitId, {
-        attributes: ['id', 'parentId'],
-        transaction
-    });
-
-    if (!unit) {
-        throw notFound('Habitación no encontrada', { code: 'UNIT_NOT_FOUND' });
-    }
-
-    if (!unit.parentId) {
-        throw badRequest('La propiedad no es una habitación', { code: 'PROPERTY_NOT_UNIT' });
-    }
-
-    const container = await Property.findByPk(unit.parentId, {
-        attributes: ['id', 'ownerId', 'isContainer'],
-        transaction
-    });
-
-    if (!container) {
-        throw notFound('Pensión/apartamento no encontrado', { code: 'CONTAINER_NOT_FOUND' });
-    }
-
-    if (!container.isContainer) {
-        throw badRequest('La propiedad padre no es una pensión/apartamento', { code: 'INVALID_CONTAINER_PARENT' });
-    }
-
-    await ensureOwnUserOrAdmin(req, container.ownerId, {
-        forbiddenCode: 'UNIT_ACCESS_FORBIDDEN',
-        forbiddenMessage
-    });
-
-    return unit;
-};
+import { assertContainerOwnership, assertUnitOwnership } from '../services/propertyOwnershipService.js';
 
 /**
  * Container Controller
